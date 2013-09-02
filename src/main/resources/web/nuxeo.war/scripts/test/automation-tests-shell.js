f246706c7e4a68d1515350e8a30bef01936a22dd
Thierry Delprat <tdelprat@nuxeo.com>
2013-09-03 01:13:07 +0200

    Integrate Mocha Unit tests with the Shell

---
+++ src/main/resources/web/nuxeo.war/scripts/test/automation-tests-shell.js
@@ -0,0 +1,48 @@
+// new Reporter for Mocha
+function NxReporter(runner, term) {
+  var passes = 0;
+  var failures = 0;
+
+  runner.on('pass', function(test){
+    passes++;
+    term.echo('[[b;#00FF00;#0] pass: ]' + test.fullTitle());
+  });
+
+  runner.on('fail', function(test, err){
+    failures++;
+    term.echo('[[b;#FF0000;#0] fail: ]' + test.fullTitle() + "[[i;#FF0000;#0]" + err.message + "]");
+  });
+
+  runner.on('end', function(){
+    if (failures==0) {
+      term.echo('[[b;#00FF00;#0] COMPLETED ] ' + passes + " / " + (passes + failures));    
+    } else {
+      term.echo('[[b;#FFFF00;#0] FAILED: ] ' + passes + " / " + (passes + failures));    
+    }    
+  });
+}
+
+// new test command
+
+/** contribute shell built-ins commands **/
+
+(function(jQuery,nuxeo) {
+  var cmds = {
+      tests  : {
+          impl : function (cmds, term, shell) {
+            mocha.checkLeaks();
+            mocha.reporter(function(reporter){NxReporter(reporter, term);});
+            mocha.globals(['jQuery']);
+            mocha.run();
+          },
+          help : "Run Automation units tests",
+          suggest : []
+      }
+  };
+
+  if (nuxeo.shell_builtins === undefined) {
+    nuxeo.shell_builtins={};
+  }
+  nuxeo.shell_builtins = jQuery.extend({}, nuxeo.shell_builtins , cmds);
+
+})(jQuery,this.nuxeo === undefined ? this.nuxeo = {} : this.nuxeo);
